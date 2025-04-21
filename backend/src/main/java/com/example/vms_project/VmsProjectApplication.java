08d5fe3de29e12ef029657d769ee7923b0937d9c
Ogulcan Kacar <ogulcankacarr@gmail.com>
2025-04-21 11:43:32 +0300

    ogulcan ilk kodunu yazdi

--- backend/src/main/java/com/example/vms_project/VmsProjectApplication.java
+++ backend/src/main/java/com/example/vms_project/VmsProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VmsProjectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("ogulcan yazdi");
 		SpringApplication.run(VmsProjectApplication.class, args);
 	}
 

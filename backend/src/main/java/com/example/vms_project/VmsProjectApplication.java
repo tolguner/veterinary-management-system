939e2eca66247ab5d88d35d550cd1abd333ad2f2
Ogulcan Kacar <ogulcankacarr@gmail.com>
2025-04-21 11:36:39 +0300

    ilk commit

---
+++ backend/src/main/java/com/example/vms_project/VmsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.vms_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmsProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/vms_project/VmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vms_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

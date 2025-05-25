430e7fa4bf67bb947f0a6b2fbdf5b1460d86ed5a
Ogulcan Kacar <ogulcankacarr@gmail.com>
2025-05-25 23:16:52 +0300

    eksik importlarin eklenmesi

--- backend/src/main/java/com/example/vms_project/repositories/VeterinaryRepository.java
+++ backend/src/main/java/com/example/vms_project/repositories/VeterinaryRepository.java
@@ -3,6 +3,9 @@ package com.example.vms_project.repositories;
 import com.example.vms_project.entities.Veterinary;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface VeterinaryRepository extends JpaRepository<Veterinary, Long> {
     List<Veterinary> findAllOrderByClinicNameAsc();
     Optional<Veterinary> findByClinicName(String clinicName);
